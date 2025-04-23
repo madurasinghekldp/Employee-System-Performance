1026f0cbf5c4a2ae6cf869a525eaff96c89e76ba
dulan <madurasinghekldp@gmail.com>
2025-04-23 08:41:46 +0530

    Remove configuration and user model files to streamline application structure

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
 from flask import Flask
 from controller.main_controller import main_bp
-from config import Config
-
-
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(Config)
     app.register_blueprint(main_bp)
 
     return app
