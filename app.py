ba71887b21acaa3ef3aa00710c5a62468e720f77
dulan <madurasinghekldp@gmail.com>
2025-04-23 13:10:09 +0530

    Add CORS support and dotenv loading to the Flask application

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from flask import Flask
 from controller.main_controller import main_bp
+from flask_cors import CORS
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 def create_app():
     app = Flask(__name__)
+    CORS(app, origins=[os.getenv('CLIENT_URL')],supports_credentials=True)
     app.register_blueprint(main_bp)
 
     return app
@@ -10,4 +16,5 @@ def create_app():
 app = create_app()
 
 if __name__ == '__main__':
+    #app.run(port=5001, debug=True)
     app.run()
